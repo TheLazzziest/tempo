c07181cfd5d0455a72d17c63c44da2feacc373bd
Max Yakovenko <mxyakovenko9@gmail.com>
2025-07-09 22:22:23 +0300

    Feature, Update docs (#3)

--- broker/urls.py
+++ broker/urls.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework.schemas import get_schema_view
+from rest_framework_json_api.schemas.openapi import SchemaGenerator
 from .users.views import UserViewSet
+
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -16,6 +20,24 @@ urlpatterns = [
     path("api-token-auth/", views.obtain_auth_token),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("silk/", include("silk.urls", namespace="silk")),
+    path(
+        "docs/openapi.json",
+        get_schema_view(
+            title="Brokern API",
+            description="API for all things …",
+            version="1.0.0",
+            generator_class=SchemaGenerator,
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "docs/",
+        TemplateView.as_view(
+            template_name="swagger.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
